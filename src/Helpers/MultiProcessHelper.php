8f39df68141c3135d9ec496285c69d78ee6a3ede
我只想看看蓝天 <1207032539@qq.com>
2020-09-14 00:19:34 +0800

    优化多进程任务助手函数

--- src/Helpers/MultiProcessHelper.php
+++ src/Helpers/MultiProcessHelper.php
@@ -11,7 +11,7 @@ class MultiProcessHelper
 
     public function __destruct()
     {
-        //等待回收所有子进程、清空配置
+        //父进程销毁前，要等待回收所有子进程、清空配置
         if (self::$pid == posix_getpid()) {
             self::recycleProcess();
         }
@@ -31,13 +31,13 @@ class MultiProcessHelper
     }
 
     /**
-     * 开多进程执行任务
+     * 开多进程执行任务第一种方式，这种方式每个任务都会新建一个子进程来执行任务，任务里的所有链接资源都是独立的，消耗资源较多
      * Author:我只想看看蓝天<1207032539@qq.com>
      * @param $function //闭包函数
      * @param array $param //数组参数
      * @throws \Exception
      */
-    public function multiProcessTask($function, $param = [])
+    public function multiProcessTask(callable $function, array $param = [])
     {
         again:
         if (self::$process_count > 0) {
@@ -59,11 +59,47 @@ class MultiProcessHelper
     }
 
     /**
+     * 开多进程执行任务第二种方式，这种方式是将任务平均分给规划的子进程去执行，一个子进程执行n个分配到的任务，
+     * 任务期间的数据库链接、redis链接可以在当前子进程下共享
+     * @param array $tasks
+     * @param callable $function
+     * @throws \Exception
+     * @author 我只想看看蓝天 <1207032539@qq.com>
+     */
+    public function multiProcessTaskV2(array $tasks, callable $function)
+    {
+        $process_tasks = [];
+        $index = 0;
+        foreach ($tasks as $task) {
+            if ($index >= self::$process_count) {
+                $index = 0;
+            }
+            $process_tasks[$index][] = $task;
+            $index++;
+        }
+        foreach ($process_tasks as $process_task) {
+            $pid = pcntl_fork();
+            if ($pid > 0) {//父进程
+                self::$childs[] = $pid;
+            } elseif ($pid == 0) { //子进程执行回调任务
+                foreach ($process_task as $item) {
+                    $function($item);
+                }
+                exit();
+            } else {
+                throw new \Exception('创建子进程失败');
+            }
+        }
+        //回收所有子进程
+        self::recycleProcess();
+    }
+
+    /**
      * 回收进程
      * @param int $hang_up
      * @author LinZhou <1207032539@qq.com>
      */
-    public static function recycleProcess($hang_up = 1)
+    public static function recycleProcess(int $hang_up = 1)
     {
         if (!empty(self::$childs)) {
             $has_stop_childs = [];
